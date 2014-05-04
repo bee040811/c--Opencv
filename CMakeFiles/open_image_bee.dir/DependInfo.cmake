392d9423e83fa599745d4fa23beae7b55f5d7ebc
bee040811 <bee040811@gmail.com>
2014-05-04 13:49:13 +0800

    cmake run opencv

---
+++ CMakeFiles/open_image_bee.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/bee040811/Downloads/opencv-2.4.8/samples/cpp/test/open_image_bee.cpp" "/Users/bee040811/Downloads/opencv-2.4.8/samples/cpp/test/CMakeFiles/open_image_bee.dir/open_image_bee.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/local/include/opencv"
+  "/usr/local/include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ CMakeFiles/open_image_bee.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/open_image_bee.dir/open_image_bee.cpp.o"
+  "open_image_bee.pdb"
+  "open_image_bee"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/open_image_bee.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
